6590c92d6f49232b0d6a5dde76b1ad272021605a
Peter Kim <seattlechem@yahoo.com>
2018-04-05 16:05:10 -0700

    for loop to populatre stock list complete

--- pyramid_stocks/pyramid_stocks/sample_data/__init__.py
+++ pyramid_stocks/pyramid_stocks/sample_data/__init__.py
@@ -1,58 +1,58 @@
 MOCK_DATA = [
             {
-                "symbol": "GE",
-                "companyName": "General Electric Company",
-                "exchange": "New York Stock Exchange",
-                "industry": "Industrial Products",
-                "website": "http://www.ge.com",
-                "description": "General Electric Co is a digital industrial \
+                'symbol': 'GE',
+                'companyName': 'General Electric Company',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Industrial Products',
+                'website': 'http://www.ge.com',
+                'description': 'General Electric Co is a digital industrial \
                  company. It operates in various segments, including power \
                  and water, oil and gas, energy management, aviation, \
                  healthcare, transportation, appliances and lighting, \
-                 and more.",
-                "CEO": "John L. Flannery",
-                "issueType": "cs",
-                "sector": "Industrials"
+                 and more.',
+                'CEO': 'John L. Flannery',
+                'issueType': 'cs',
+                'sector': 'Industrials'
             },
             {
-                "symbol": "AA",
-                "companyName": "Alcoa Corp",
-                "exchange": "New York Stock Exchange",
-                "industry": "Metal Fabrications",
-                "website": "https://www.alcoa.com/global/en/home.asp",
-                "description": "Nunc tincidunt vitae diam at mollis. \
+                'symbol': 'AA',
+                'companyName': 'Alcoa Corp',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Metal Fabrications',
+                'website': 'https://www.alcoa.com/global/en/home.asp',
+                'description': 'Nunc tincidunt vitae diam at mollis. \
                 Pellentesque luctus massa nec odio mollis feugiat. Praesent \
                 vitae sapien quis diam viverra porttitor quis quis odio. \
                 Suspendisse potenti. Donec vel metus sed nisl ultrices \
                 ultricies non et mi. Suspendisse aliquam erat dolor, ac \
                 semper nisi interdum vitae. Aliquam erat volutpat. \
                 Aliquam quam libero, venenatis ac tortor ac, mattis \
-                gravida lacus.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Capital Goods"
+                gravida lacus.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Capital Goods'
             },
             {
-                "symbol": "ABT",
-                "companyName": "Abbott Laboratories",
-                "exchange": "New York Stock Exchange",
-                "industry": "Major Pharmaceuticals",
-                "website": "http://www.abbott.com",
-                "description": "Abbott Laboratories is an Illinois corporation,\
-                 incorporated in 1900.  Abbott's* principal business is the\
+                'symbol': 'ABT',
+                'companyName': 'Abbott Laboratories',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Major Pharmaceuticals',
+                'website': 'http://www.abbott.com',
+                'description': 'Abbott Laboratories is an Illinois corporation,\
+                 incorporated in 1900.  Abbott\'s* principal business is the\
                  discovery, development, manufacture, and  sale of a broad \
-                 and diversified line of health care products.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Consumer Durables"
+                 and diversified line of health care products.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Consumer Durables'
             },
             {
-                "symbol": "AET",
-                "companyName": "Aetna Inc",
-                "exchange": "New York Stock Exchange",
-                "industry": "Hospital And Medical Service Plans",
-                "website": "https://www.aetna.com",
-                "description": "We are one of the nation?s leading diversified \
+                'symbol': 'AET',
+                'companyName': 'Aetna Inc',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Hospital And Medical Service Plans',
+                'website': 'https://www.aetna.com',
+                'description': 'We are one of the nation?s leading diversified \
                  health care benefits companies,  serving approximately 36.7 \
                  million people with information and resources to help  them \
                  make better informed decisions about their health care. We \
@@ -60,34 +60,34 @@ MOCK_DATA = [
                  health insurance products and related  services, including \
                  medical, pharmacy, dental, behavioral health, group life \
                  and disability plans, and medical management capabilities \
-                 and health care management  services for Medicaid plans.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Health Care"
+                 and health care management  services for Medicaid plans.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Health Care'
             },
             {
-                "symbol": "OMI",
-                "companyName": "Owens & Minor",
-                "exchange": "New York Stock Exchange",
-                "industry": "Medical Specialities",
-                "website": "",
-                "description": "Owens & Minor, Inc. and subsidiaries (Owens & \
+                'symbol': 'OMI',
+                'companyName': 'Owens & Minor',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Medical Specialities',
+                'website': 'https://www.owens-minor.com/',
+                'description': 'Owens & Minor, Inc. and subsidiaries (Owens & \
                  Minor, O&M or the company) is the  nation?s leading \
                  distributor of medical and surgical supplies to the \
                  acute-care  market, a healthcare supply-chain management \
                  company, and a national  direct-to-consumer supplier of \
-                 testing and monitoring supplies for diabetics.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Health Care"
+                 testing and monitoring supplies for diabetics.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Health Care'
             },
             {
-                "symbol": "SAN",
-                "companyName": "Banco Santander",
-                "exchange": "New York Stock Exchange",
-                "industry": "Commercial Banks",
-                "website": "https://www.bancosantander.es/es/particulares",
-                "description": "We were formed on August 1, 2002 through the \
+                'symbol': 'SAN',
+                'companyName': 'Banco Santander',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Commercial Banks',
+                'website': 'https://www.bancosantander.es/es/particulares',
+                'description': 'We were formed on August 1, 2002 through the \
                  merger of   Santiago and Old  Santander-Chile, both of which \
                  were subsidiaries of our controlling shareholder,  Banco \
                  Santander Central Hispano. We are the largest bank in Chile \
@@ -97,18 +97,18 @@ MOCK_DATA = [
                  net of allowances outstanding of Ch$11,614,895 million \
                  (US$21,733 million),  deposits of Ch$9,392,332 million \
                  (US$17,574 million) and shareholders? equity of  \
-                 Ch$1,245,339 million (US$2,330 million).",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Finance"
+                 Ch$1,245,339 million (US$2,330 million).',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Finance'
             },
             {
-                "symbol": "WAT",
-                "companyName": "Waters Corp",
-                "exchange": "New York Stock Exchange",
-                "industry": "Biotechnology: Laboratory Analytical Instruments",
-                "website": "http://www.waters.com/waters/home.htm?locale=en_US",
-                "description": "Waters Corporation (?Waters? or the ?Company?), \
+                'symbol': 'WAT',
+                'companyName': 'Waters Corp',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Biotechnology: Laboratory Analytical Instruments',
+                'website': 'http://www.waters.com/waters/home.htm?locale=en_US',
+                'description': 'Waters Corporation (?Waters? or the ?Company?), \
                  an analytical instrument  manufacturer, designs, \
                  manufactures, sells and services, through its Waters  \
                  Division, high performance liquid chromatography \
@@ -117,34 +117,34 @@ MOCK_DATA = [
                  and mass spectrometry (?MS?) instrument systems and support \
                  products,  including chromatography columns, other \
                  consumable products and comprehensive  post-warranty \
-                 service plans.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Capital Goods"
+                 service plans.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Capital Goods'
             },
             {
-                "symbol": "ZF",
-                "companyName": "Zweig Fund",
-                "exchange": "New York Stock Exchange",
-                "industry": "Closed-End Fund - Debt",
-                "website": "http://ir.virtus.com/",
-                "description": "Prior to the open of trading on the NYSE today, \
+                'symbol': 'ZF',
+                'companyName': 'Zweig Fund',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Closed-End Fund - Debt',
+                'website': 'http://ir.virtus.com/',
+                'description': 'Prior to the open of trading on the NYSE today, \
                 each share of DCA common stock converted into an equivalent \
                 dollar amount (to the nearest $0.0001) of shares of \
                 common stock of ZF. The conversion price was based on \
-                each fund's net asset value (NAV) per share calculated \
-                at the close of business on Friday, March 31, 2017:",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Finance"
+                each fund\'s net asset value (NAV) per share calculated \
+                at the close of business on Friday, March 31, 2017:',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Finance'
             },
             {
-                "symbol": "ZEN",
-                "companyName": "Zendesk Inc",
-                "exchange": "New York Stock Exchange",
-                "industry": "Consulting",
-                "website": "https://www.zendesk.com/about/",
-                "description": "Donec laoreet purus bibendum nisl \
+                'symbol': 'ZEN',
+                'companyName': 'Zendesk Inc',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Consulting',
+                'website': 'https://www.zendesk.com/about/',
+                'description': 'Donec laoreet purus bibendum nisl \
                  tincidunt malesuada eget sit amet nisi. Nunc feugiat ac \
                  orci eget condimentum. Vestibulum eleifend massa nisi, \
                  vitae sagittis tellus pharetra ac. Aenean purus metus, \
@@ -152,27 +152,27 @@ MOCK_DATA = [
                  quis turpis sit amet lobortis. Curabitur consectetur \
                  facilisis mauris, at convallis ante elementum in. \
                  Sed pellentesque, dolor ut posuere accumsan, eros \
-                 tortor fringilla sem, non ultrices eros turpis non odio.",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Consulting"
+                 tortor fringilla sem, non ultrices eros turpis non odio.',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Consulting'
             },
             {
-                "symbol": "NCA",
-                "companyName": "Nuveen California Muni Value Fund",
-                "exchange": "New York Stock Exchange",
-                "industry": "Closed-End Fund - Debt",
-                "website": "https://www.nuveen.com/closed-end-funds/NCA",
-                "description": "The fund invests in municipal securities \
+                'symbol': 'NCA',
+                'companyName': 'Nuveen California Muni Value Fund',
+                'exchange': 'New York Stock Exchange',
+                'industry': 'Closed-End Fund - Debt',
+                'website': 'https://www.nuveen.com/closed-end-funds/NCA',
+                'description': 'The fund invests in municipal securities \
                  that are exempt from federal and California state \
                  income taxes. The fund invests at least 80% of its \
                  managed assets in securities rated, at the time of \
                  investment, investment grade (Baa/BBB or better by \
                  S&P, Moody’s, or Fitch) or, if they are unrated, are \
-                 judged by the manager to be of comparable quality. ",
-                "CEO": "Lorem ipsum",
-                "issueType": "cs",
-                "sector": "Finance"
+                 judged by the manager to be of comparable quality. ',
+                'CEO': 'Lorem ipsum',
+                'issueType': 'cs',
+                'sector': 'Finance'
             }
 
 ]
