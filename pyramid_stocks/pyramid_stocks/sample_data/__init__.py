a51eaa364cf3af41b83be5c44a8975904011a852
Peter Kim <seattlechem@yahoo.com>
2018-04-05 15:26:45 -0700

    sameple data folder and init.py created

---
+++ pyramid_stocks/pyramid_stocks/sample_data/__init__.py
@@ -0,0 +1,140 @@
+MOCK_DATA = [
+            {
+                "symbol": "GE",
+                "companyName": "General Electric Company",
+                "exchange": "New York Stock Exchange",
+                "industry": "Industrial Products",
+                "website": "http://www.ge.com",
+                "description": "General Electric Co is a digital industrial \
+                 company. It operates in various segments, including power \
+                 and water, oil and gas, energy management, aviation, \
+                 healthcare, transportation, appliances and lighting, \
+                 and more.",
+                "CEO": "John L. Flannery",
+                "issueType": "cs",
+                "sector": "Industrials"
+            },
+            {
+                "symbol": "AA",
+                "companyName": "Alcoa Corp",
+                "exchange": "New York Stock Exchange",
+                "industry": "Metal Fabrications",
+                "website": "https://www.alcoa.com/global/en/home.asp",
+                "description": "Nunc tincidunt vitae diam at mollis. \
+                Pellentesque luctus massa nec odio mollis feugiat. Praesent \
+                vitae sapien quis diam viverra porttitor quis quis odio. \
+                Suspendisse potenti. Donec vel metus sed nisl ultrices \
+                ultricies non et mi. Suspendisse aliquam erat dolor, ac \
+                semper nisi interdum vitae. Aliquam erat volutpat. \
+                Aliquam quam libero, venenatis ac tortor ac, mattis \
+                gravida lacus.",
+                "CEO": "Lorem ipsum",
+                "issueType": "cs",
+                "sector": "Capital Goods"
+            },
+            {
+                "symbol": "ABT",
+                "companyName": "Abbott Laboratories",
+                "exchange": "New York Stock Exchange",
+                "industry": "Major Pharmaceuticals",
+                "website": "http://www.abbott.com",
+                "description": "Abbott Laboratories is an Illinois corporation,\
+                 incorporated in 1900.  Abbott's* principal business is the\
+                 discovery, development, manufacture, and  sale of a broad \
+                 and diversified line of health care products.",
+                "CEO": "Lorem ipsum",
+                "issueType": "cs",
+                "sector": "Consumer Durables"
+            },
+            {
+                "symbol": "AET",
+                "companyName": "Aetna Inc",
+                "exchange": "New York Stock Exchange",
+                "industry": "Hospital And Medical Service Plans",
+                "website": "https://www.aetna.com",
+                "description": "We are one of the nation?s leading diversified \
+                 health care benefits companies,  serving approximately 36.7 \
+                 million people with information and resources to help  them \
+                 make better informed decisions about their health care. We \
+                 offer a broad  range of traditional and consumer-directed \
+                 health insurance products and related  services, including \
+                 medical, pharmacy, dental, behavioral health, group life \
+                 and disability plans, and medical management capabilities \
+                 and health care management  services for Medicaid plans.",
+                "CEO": "Lorem ipsum",
+                "issueType": "cs",
+                "sector": "Health Care"
+            },
+            {
+                "symbol": "OMI",
+                "companyName": "Owens & Minor",
+                "exchange": "New York Stock Exchange",
+                "industry": "Medical Specialities",
+                "website": "",
+                "description": "Owens & Minor, Inc. and subsidiaries (Owens & \
+                 Minor, O&M or the company) is the  nation?s leading \
+                 distributor of medical and surgical supplies to the \
+                 acute-care  market, a healthcare supply-chain management \
+                 company, and a national  direct-to-consumer supplier of \
+                 testing and monitoring supplies for diabetics.",
+                "CEO": "Lorem ipsum",
+                "issueType": "cs",
+                "sector": "Health Care"
+            },
+            {
+                "symbol": "",
+                "companyName": "",
+                "exchange": "New York Stock Exchange",
+                "industry": "",
+                "website": "",
+                "description": "",
+                "CEO": "Lorem ipsum",
+                "issueType": "",
+                "sector": ""
+            },
+            {
+                "symbol": "",
+                "companyName": "",
+                "exchange": "New York Stock Exchange",
+                "industry": "",
+                "website": "",
+                "description": "",
+                "CEO": "Lorem ipsum",
+                "issueType": "",
+                "sector": ""
+            },
+            {
+                "symbol": "",
+                "companyName": "",
+                "exchange": "New York Stock Exchange",
+                "industry": "",
+                "website": "",
+                "description": "",
+                "CEO": "Lorem ipsum",
+                "issueType": "",
+                "sector": ""
+            },
+            {
+                "symbol": "",
+                "companyName": "",
+                "exchange": "New York Stock Exchange",
+                "industry": "",
+                "website": "",
+                "description": "",
+                "CEO": "Lorem ipsum",
+                "issueType": "",
+                "sector": ""
+            },
+            {
+                "symbol": "",
+                "companyName": "",
+                "exchange": "New York Stock Exchange",
+                "industry": "",
+                "website": "",
+                "description": "",
+                "CEO": "Lorem ipsum",
+                "issueType": "",
+                "sector": ""
+            }
+
+]
