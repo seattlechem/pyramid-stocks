7325b76b49993b0a9df5b9875f9ca330498a37a4
Peter Kim <seattlechem@yahoo.com>
2018-04-10 18:39:59 -0700

    no permission required add in home view config

--- pyramid_stocks/pyramid_stocks/views/default.py
+++ pyramid_stocks/pyramid_stocks/views/default.py
@@ -1,7 +1,11 @@
 from pyramid.view import view_config
+from pyramid.security import NO_PERMISSION_REQUIRED
 
 
-@view_config(route_name='home', renderer='../templates/base.jinja2')
+@view_config(
+    route_name='home',
+    renderer='../templates/base.jinja2',
+    permission=NO_PERMISSION_REQUIRED)
 def my_view(request):
     """ Route back to homepage """
     return {}
