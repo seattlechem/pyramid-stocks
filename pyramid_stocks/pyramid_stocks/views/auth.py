7e9bcace58b8a7a7160e6791c6b808e495e6673d
Peter Kim <seattlechem@yahoo.com>
2018-04-11 20:46:18 -0700

    auth view completed

--- pyramid_stocks/pyramid_stocks/views/auth.py
+++ pyramid_stocks/pyramid_stocks/views/auth.py
@@ -1,10 +1,8 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.security import NO_PERMISSION_REQUIRED, remember, forget
-from ..sample_data import MOCK_DATA
 from sqlalchemy.exc import DBAPIError
 from pyramid.httpexceptions import HTTPFound, HTTPBadRequest, HTTPUnauthorized
-import requests
 from . import DB_ERR_MSG
 from ..models import Account
 
@@ -64,64 +62,3 @@ def register_page(request):
 def logout(request):
     headers = forget(request)
     return HTTPFound(location=request.route_url('home'), headers=headers)
-
-
-@view_config(route_name='stock', renderer='../templates/stock_add.jinja2')
-def searching_stock_ticker(request):
-    """ searching for a stock ticker symbol """
-    if request.method == 'GET':
-        try:
-            symbol = request.GET['symbol']
-        except KeyError:
-            return {}
-        address = 'https://api.iextrading.com/1.0/\
-stock/{}/company'.format(symbol)
-        response = requests.get(address)
-        response = response.json()
-
-        return {'entry': response}
-
-    elif request.method == 'POST':
-        symbol = request.POST['new_stock']
-        symbol = symbol.lower()
-        for entries in MOCK_DATA:
-            for val in entries.values():
-                # import pdb; pdb.set_trace()
-                if val == symbol.upper():
-                    return HTTPFound(location=request.route_url('portfolio'))
-
-        address = 'https://api.iextrading.com/1.0/stock/{}/\
-company'.format(symbol)
-        response = requests.get(address)
-        response = response.json()
-        MOCK_DATA.append(
-            {'symbol': response['symbol'],
-                'companyName': response['companyName'],
-                'exchange': response['exchange'],
-                'industry': response['industry'],
-                'website': response['website'],
-                'description': response['description'],
-                'CEO': response['CEO'],
-                'issueType': response['issueType'],
-                'sector': response['sector']})
-        return HTTPFound(location=request.route_url('portfolio'))
-    else:
-        return HTTPNotFound()
-
-
-@view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2',
-             request_method='GET')
-def view_existing_stocks(request):
-    """ display user's existing stocks"""
-    return {'entries': MOCK_DATA}
-
-
-@view_config(route_name='detail', renderer='../templates/stock_detail.jinja2',
-             request_method='GET')
-def get_detail_view(request):
-    """ detail about a user's existing stock """
-    symbol = request.matchdict['symbol']
-    for entry in MOCK_DATA:
-        if entry['symbol'] == symbol:
-            return {'result': entry}
-    return {}
